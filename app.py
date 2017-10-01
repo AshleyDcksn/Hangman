b1c4e759e33a83bf0b55a5a75b48558bac79376a
AshleyDcksn <dicksona@mail.usf.edu>
2017-09-30 23:02:25 -0400

    Adding imgs for game plus started outlining routes and fragments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +10,17 @@ def hello_world():
 @app.route('/new_game')
 def start_new_game():
     return render_template('new_game.html')
+
+#TODO Add method for user to submit a char
+#@app.route('/add', methods=['POST'])
+#def add_char():
+#    my_char = request.form['char']
+#    return redirect(url_for('show_entries'))
+#TODO Add method to return template based on char
+#@app.route('/')
+#def show_entries():
+#    entries = cur.fetchall()
+#    return render_template('show_entries.html', entries=entries)
 
 @app.route('/guess_letter/<string:letter>')
 def guess_letter():
