21b4b13bd3b9c3bb6db4d8ae103201acdd8cfd95
AshleyDcksn <dicksona@mail.usf.edu>
2017-10-12 21:24:11 -0400

    Added more logic to handle checking if letter is in word. Made Hangman obj global to persist it through calls, added logic for hangman figure

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,12 @@
 __author__ = 'Ashley'
-class Hangman:
-    word=""
-    word_length=0
-    number_of_tries = -1
+class NewHangmanGame:
+  #  word=""
+  #  word_length=0
+  #  number_of_tries = -1
 
-    def function(self, word, word_length):
-        self.word = word
-        self.word_length = word_length
-        self.number_of_tries = 7
+    def __init__(self, _id, _word, _word_length):
+        self.id = _id
+        self.word = _word
+        self.word_length = _word_length
+        self.number_of_tries = 6
+        self.index_list = []
--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, session
 from flask import render_template
 from flask import request
-from flask import redirect
-from flask import url_for
 from flask.ext.mysql import MySQL
+import os
 import random
 import configureDB
 import Hangman
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 mysql = MySQL()
 configureDB.configure(app)
 mysql.init_app(app)
+hmGame = None
+
 
 @app.route('/')
 def hello_world():
@@ -22,16 +25,26 @@ def start_new_game():
     conn = mysql.connect()
     cursor = mysql.get_db().cursor()
     cursor.execute("SELECT word, word_length FROM dev.hang_man_tbl where idhang_man=(%s)",(word_index))
-    row = cursor.fetchone()
-    #hangman_game = Hangman(row[0],row[1])
+    row = list(cursor.fetchone())
+    global hmGame
+    hmGame = Hangman.NewHangmanGame(word_index, row[0],row[1])
     conn.close()
-    return render_template('new_game.html', word=row)
+    return render_template('new_game.html', hm=hmGame)
 
 #TODO Add method for user to submit a char
 @app.route('/add_char', methods=['POST'])
 def add_char():
-    my_char = request.form['letter']
-    return redirect(url_for('show_entries'))
+    letter = request.form['letter']
+    global hmGame
+    hmWord = hmGame.word
+    letter_indexes = [pos for pos, char in enumerate(hmWord) if char == letter]
+    if len(letter_indexes) == 0:
+        hmGame.number_of_tries -= 1
+    else:
+        hmGame.index_list.extend(letter_indexes)
+    return render_template('game_1.html', hm=hmGame)
+
+
 
 #TODO Add method to return template based on char
 @app.route('/show_game_1')
